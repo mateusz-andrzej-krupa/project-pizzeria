8c6115c9e77cea6d657531c805c00a927171a70a
Mateusz Krupa <mateuszandrzejkrupa@gmail.com>
2020-10-23 23:03:19 +0200

    Add activatePage and links

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,36 @@
-import {settings, select} from './settings.js';
+import {settings, select, classNames} from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
 
 const app = {
+
+  initPages: function(){
+    const thisApp = this;
+
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+    
+    thisApp.activatePage(thisApp.pages[0].id);
+  },
+
+  activatePage: function(pageId) {
+    const thisApp = this;
+
+    /* add class active to matching pages, remove from non-matchinch */
+    for (let page of thisApp.pages) {
+      page.classList.add(classNames.pages.active, page.Id == pageId); //try toggle
+      console.log(page);
+    }
+    /* add class active to matching link, remove from non-matchinch */
+    for (let link of thisApp.navLinks) {
+      link.classList.toggle(
+        classNames.nav.active, 
+        link.getAttribute('href') == '#' + pageId
+      );
+    }
+
+  },
 
   initMenu: function(){
     const thisApp = this;
@@ -45,8 +73,10 @@ const app = {
   init: function(){
     const thisApp = this;
     console.log('*** App starting ***');
+    thisApp.initPages();
     thisApp.initData();
     thisApp.initCart();
+    
   },
 };
 
