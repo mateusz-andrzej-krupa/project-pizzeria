d46e9be8790056d6a23e32b2b786f3632cbd41f2
Mateusz Krupa <mateuszandrzejkrupa@gmail.com>
2020-10-24 14:34:15 +0200

    Add pages handler

--- src/js/app.js
+++ src/js/app.js
@@ -8,28 +8,49 @@ const app = {
     const thisApp = this;
 
     thisApp.pages = document.querySelector(select.containerOf.pages).children;
-    
     thisApp.navLinks = document.querySelectorAll(select.nav.links);
     
-    thisApp.activatePage(thisApp.pages[0].id);
+    const idFromHash = window.location.hash.replace('#/', '');
+    
+    let pageMatchingHash = thisApp.pages[0].id;
+    
+    for (let page of thisApp.pages){
+      if (page.id == idFromHash){
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+
+    thisApp.activatePage(pageMatchingHash);
+
+    for (let link of thisApp.navLinks){
+      link.addEventListener('click', function(event){
+        const clickedElement = this;
+        event.preventDefault();
+
+        const id = clickedElement.getAttribute('href').replace('#', '');
+        thisApp.activatePage(id);
+
+        window.location.hash = `#/${ id }`;
+      });
+    }
+
   },
 
-  activatePage: function(pageId) {
+
+  activatePage: function(pageId){
     const thisApp = this;
 
-    /* add class active to matching pages, remove from non-matchinch */
-    for (let page of thisApp.pages) {
-      page.classList.add(classNames.pages.active, page.Id == pageId); //try toggle
-      console.log(page);
-    }
-    /* add class active to matching link, remove from non-matchinch */
-    for (let link of thisApp.navLinks) {
-      link.classList.toggle(
-        classNames.nav.active, 
-        link.getAttribute('href') == '#' + pageId
-      );
+    for (let page of thisApp.pages){
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
     }
 
+    for (let link of thisApp.navLinks){
+      link.classList.toggle(
+        classNames.nav.active, 
+        link.getAttribute('href') == `#${ pageId }`
+      );
+    }
   },
 
   initMenu: function(){
@@ -64,7 +85,7 @@ const app = {
     thisApp.cart = new Cart(cartElem);
 
     thisApp.productList = document.querySelector(select.containerOf.menu);
-    thisApp.productList.addEventListener('add-to-cart', function(event) {
+    thisApp.productList.addEventListener('add-to-cart', function(event){
       app.cart.add(event.detail.product);
       
     });
